2333d8203f60c2a06022ea1020b45b1f6ccbbe89
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 03:07:51 +0800

    更新 ble_gateway.h

--- components/ble_gateway/ble_gateway.h
+++ components/ble_gateway/ble_gateway.h
@@ -1,11 +1,7 @@
 #pragma once
 
 
-#include "esphome.h"
-
-#include "esp_gap_ble_api.h"
-#include "esp_bt.h"
-#include "esp_bt_main.h"
+#include "esphome/core/component.h"
 
 
 namespace esphome {
@@ -15,27 +11,11 @@ namespace ble_gateway {
 class BLEGateway : public Component {
 
 
-public:
+ public:
 
+  void setup() override;
 
-void setup() override;
-
-
-void loop() override;
-
-
-
-void send_hex(
-    const std::string &hex
-);
-
-
-
-static void gap_callback(
-    esp_gap_ble_cb_event_t event,
-    esp_ble_gap_cb_param_t *param
-);
-
+  void loop() override;
 
 
 };
