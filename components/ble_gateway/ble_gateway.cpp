2c2cf48cdf6e9133aed1634faf48c47768f701d1
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 03:44:11 +0800

    更新 ble_gateway.cpp

--- components/ble_gateway/ble_gateway.cpp
+++ components/ble_gateway/ble_gateway.cpp
@@ -1,46 +1,85 @@
-#pragma once
+#include "ble_gateway.h"
 
+#include "esphome/core/log.h"
 
-#include "esphome/core/component.h"
-
-#include <string>
-#include <vector>
+#include "esp_gap_ble_api.h"
+#include "esp_bt.h"
 
 
 namespace esphome {
 namespace ble_gateway {
 
 
-
-class BLEGateway : public Component {
-
-
- public:
+static const char *TAG = "ble_gateway";
 
 
-  void setup() override;
+void BLEGateway::setup()
+{
+
+  ESP_LOGI(
+      TAG,
+      "BLE Gateway start"
+  );
+
+}
 
 
-  void loop() override;
+void BLEGateway::loop()
+{
+
+}
 
 
-  void send_hex(
-      std::string hex
+
+std::vector<uint8_t>
+BLEGateway::hex_to_bytes(
+    const std::string &hex
+)
+
+{
+
+  std::vector<uint8_t> data;
+
+
+  for(
+      size_t i = 0;
+      i + 1 < hex.length();
+      i += 2
+  )
+
+  {
+
+    data.push_back(
+        strtol(
+            hex.substr(i,2).c_str(),
+            nullptr,
+            16
+        )
+    );
+
+  }
+
+
+  return data;
+
+}
+
+
+
+void BLEGateway::send_hex(
+    std::string hex
+)
+
+{
+
+  ESP_LOGI(
+      TAG,
+      "send:%s",
+      hex.c_str()
   );
 
 
-
- private:
-
-
-  std::vector<uint8_t> hex_to_bytes(
-      const std::string &hex
-  );
-
-
-
-};
-
+}
 
 
 }
