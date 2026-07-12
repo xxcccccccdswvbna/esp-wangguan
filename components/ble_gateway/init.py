f1f16f21509b209e00286f9d5df11c3c7f4a7486
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 04:12:48 +0800

    更新 init.py

--- components/ble_gateway/init.py
+++ components/ble_gateway/init.py
@@ -1,45 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
+from esphome.components import mqtt
 from esphome.const import CONF_ID
 
+DEPENDENCIES = ["esp32_ble_tracker"]
 
-DEPENDENCIES = [
-    "esp32"
-]
+ble_gateway_ns = cg.esphome_ns.namespace("ble_gateway")
 
-
-AUTO_LOAD = [
-    "esp32_ble"
-]
-
-
-ble_gateway_ns = cg.esphome_ns.namespace(
-    "ble_gateway"
-)
-
-
-BLEGateway = ble_gateway_ns.class_(
-    "BLEGateway",
-    cg.Component
-)
-
+BLEGateway = ble_gateway_ns.class_("BLEGateway", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID():
-        cv.declare_id(BLEGateway),
+        cv.GenerateID(): cv.declare_id(BLEGateway),
     }
 )
-
-
-async def to_code(config):
-
-    var = cg.new_Pvariable(
-        config[CONF_ID]
-    )
-
-    await cg.register_component(
-        var,
-        config
-    )
