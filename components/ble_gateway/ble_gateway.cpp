a2afc32d26135e407e35e5197be2cffbc7e684da
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 03:39:20 +0800

    更新 ble_gateway.cpp

--- components/ble_gateway/ble_gateway.cpp
+++ components/ble_gateway/ble_gateway.cpp
@@ -1,140 +1,46 @@
-#include "ble_gateway.h"
+#pragma once
 
 
-#include "esp_gap_ble_api.h"
-#include "esp_bt.h"
+#include "esphome/core/component.h"
+
+#include <string>
+#include <vector>
 
 
 namespace esphome {
 namespace ble_gateway {
 
 
-static const char *TAG =
-"BLE_GATEWAY";
+
+class BLEGateway : public Component {
 
 
+ public:
 
-void BLEGateway::setup()
-{
 
-  ESP_LOGI(
-      TAG,
-      "BLE RAW Gateway started"
+  void setup() override;
+
+
+  void loop() override;
+
+
+  void send_hex(
+      std::string hex
   );
 
 
-}
+
+ private:
 
 
-
-void BLEGateway::loop()
-{
-
-}
-
-
-
-std::vector<uint8_t>
-BLEGateway::hex_to_bytes(
-    const std::string &hex
-)
-{
-
-  std::vector<uint8_t> data;
-
-
-  for(
-      size_t i=0;
-      i+1<hex.length();
-      i+=2
-  )
-  {
-
-    data.push_back(
-        strtol(
-            hex.substr(i,2).c_str(),
-            nullptr,
-            16
-        )
-    );
-
-  }
-
-
-  return data;
-
-}
-
-
-
-void BLEGateway::send_hex(
-    std::string hex
-)
-{
-
-
-  auto data =
-      hex_to_bytes(hex);
-
-
-
-  if(data.empty())
-  {
-
-    ESP_LOGW(
-        TAG,
-        "empty packet"
-    );
-
-    return;
-
-  }
-
-
-
-  esp_ble_gap_config_adv_data_raw(
-      data.data(),
-      data.size()
+  std::vector<uint8_t> hex_to_bytes(
+      const std::string &hex
   );
 
 
-  esp_ble_adv_params_t params{};
 
+};
 
-  params.adv_int_min =
-      0x20;
-
-
-  params.adv_int_max =
-      0x40;
-
-
-  params.adv_type =
-      ADV_TYPE_NONCONN_IND;
-
-
-  params.channel_map =
-      ADV_CHNL_ALL;
-
-
-  params.adv_filter_policy =
-      ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY;
-
-
-
-  esp_ble_gap_start_advertising(
-      &params
-  );
-
-
-  ESP_LOGI(
-      TAG,
-      "TX:%s",
-      hex.c_str()
-  );
-
-
-}
 
 
 }
