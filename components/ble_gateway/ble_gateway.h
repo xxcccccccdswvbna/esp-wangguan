cceb2178c25265cdf8753f8dd3b61299282ce556
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 03:31:47 +0800

    更新 ble_gateway.h

--- components/ble_gateway/ble_gateway.h
+++ components/ble_gateway/ble_gateway.h
@@ -1,24 +1,46 @@
 #pragma once
 
 
-#include "esphome/core/component.h"
+#include "esphome.h"
+
+#include <vector>
+#include <string>
 
 
 namespace esphome {
 namespace ble_gateway {
 
 
+
 class BLEGateway : public Component {
 
 
- public:
+public:
 
-  void setup() override;
 
-  void loop() override;
+    void setup() override;
+
+
+    void loop() override;
+
+
+    void send_hex(std::string hex);
+
+
+
+private:
+
+
+    void start_scan();
+
+
+    std::vector<uint8_t> hex_to_bytes(
+        const std::string &hex
+    );
 
 
 };
+
 
 
 }
