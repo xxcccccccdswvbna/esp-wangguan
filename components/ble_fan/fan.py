64f057c6c57be9f47e7d46d7c9000cc355a6ab0e
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-14 01:58:16 +0800

    Update fan.py

--- components/ble_fan/fan.py
+++ components/ble_fan/fan.py
@@ -9,21 +9,17 @@ ble_fan_ns = cg.esphome_ns.namespace("ble_fan")
 ble_gateway_ns = cg.esphome_ns.namespace("ble_gateway")
 fan_ns = cg.esphome_ns.namespace("fan")
 
-# 【核心】只绑定 fan.Fan，绝对不要加 cg.Component！
-BLEFan = ble_fan_ns.class_("BLEFan", fan.Fan)
+# 【关键】必须同时声明 cg.Component 和 fan.Fan
+BLEFan = ble_fan_ns.class_("BLEFan", cg.Component, fan.Fan)
 BLEGateway = ble_gateway_ns.class_("BLEGateway", cg.Component)
 
 CONF_BLE_DEVICE_ID = "ble_device_id"
 CONF_GATEWAY = "gateway"
 
 FanRestoreMode = fan_ns.namespace("FanRestoreMode")
-
 RESTORE_MODES = {
-    "NO_RESTORE": FanRestoreMode.NO_RESTORE,
     "RESTORE_DEFAULT_OFF": FanRestoreMode.RESTORE_DEFAULT_OFF,
     "RESTORE_DEFAULT_ON": FanRestoreMode.RESTORE_DEFAULT_ON,
-    "RESTORE_INVERTED_DEFAULT_OFF": FanRestoreMode.RESTORE_INVERTED_DEFAULT_OFF,
-    "RESTORE_INVERTED_DEFAULT_ON": FanRestoreMode.RESTORE_INVERTED_DEFAULT_ON,
     "ALWAYS_OFF": FanRestoreMode.ALWAYS_OFF,
     "ALWAYS_ON": FanRestoreMode.ALWAYS_ON,
 }
@@ -39,7 +35,8 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     
-    # 【核心】不要手动 register_component！fan.register_fan 内部已经完美处理了组件注册和生命周期！
+    # 【关键】必须注册为 Component
+    await cg.register_component(var, config)
     await fan.register_fan(var, config)
 
     gateway_var = await cg.get_variable(config[CONF_GATEWAY])
