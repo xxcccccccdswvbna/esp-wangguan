f4cc64ec161998f839726be685bff1972c0fec25
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 12:11:07 +0800

    Update __init__.py

--- components/ble_gateway/__init__.py
+++ components/ble_gateway/__init__.py
@@ -1,37 +1,5 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
 
-from esphome.const import CONF_ID
-
-
-CODEOWNERS = ["@ct1"]
-
-DEPENDENCIES = ["esp32"]
-
+CODEOWNERS = []
 
 ble_gateway_ns = cg.esphome_ns.namespace("ble_gateway")
-
-BLEGateway = ble_gateway_ns.class_(
-    "BLEGateway",
-    cg.Component
-)
-
-
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_ID):
-        cv.declare_id(BLEGateway),
-    }
-)
-
-
-async def to_code(config):
-
-    var = cg.new_Pvariable(
-        config[CONF_ID]
-    )
-
-    await cg.register_component(
-        var,
-        config
-    )
