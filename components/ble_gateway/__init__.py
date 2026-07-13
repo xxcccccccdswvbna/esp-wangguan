ad60a4e85ef286342f33d6c9f4e1a330363ecd0a
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 12:59:05 +0800

    Update __init__.py

--- components/ble_gateway/__init__.py
+++ components/ble_gateway/__init__.py
@@ -20,3 +20,15 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(): cv.declare_id(BLEGateway),
     }
 )
+
+
+async def to_code(config):
+
+    var = cg.new_Pvariable(
+        config[CONF_ID]
+    )
+
+    await cg.register_component(
+        var,
+        config
+    )
