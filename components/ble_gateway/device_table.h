1a0da2b97f84be5c0c0dee56810398a327b23e7c
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 19:18:04 +0800

    更新 device_table.h

--- components/ble_gateway/device_table.h
+++ components/ble_gateway/device_table.h
@@ -1,13 +1,42 @@
 #pragma once
 
-
-#include "device_model.h"
-
+#include <string>
 #include <vector>
+#include <map>
 
 
 namespace esphome {
 namespace ble_gateway {
+
+
+struct BLEAction
+{
+
+    std::string name;
+
+
+    std::vector<std::string> packets;
+
+};
+
+
+
+struct BLEDevice
+{
+
+    std::string id;
+
+
+    std::string type;
+
+
+    std::string name;
+
+
+    std::map<std::string, BLEAction> actions;
+
+};
+
 
 
 class DeviceTable
@@ -15,8 +44,28 @@ class DeviceTable
 
 public:
 
+
     static void load(
         std::vector<BLEDevice> &devices
+    );
+
+
+private:
+
+
+    static void add_device(
+        std::vector<BLEDevice> &devices,
+        std::string id,
+        std::string type,
+        std::string name
+    );
+
+
+    static void add_action(
+        std::vector<BLEDevice> &devices,
+        std::string device_id,
+        std::string action,
+        std::vector<std::string> packets
     );
 
 
