addc6ba90dd75f9366ec124df9c02706bfce8386
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 10:49:52 +0800

    Update config_manager.h

--- components/ble_gateway/config_manager.h
+++ components/ble_gateway/config_manager.h
@@ -1,44 +1,123 @@
-#pragma once
+#include "config_manager.h"
 
+#include "device_table.h"
 
-#include "device_model.h"
-
-#include <vector>
-#include <string>
-
+#include "esphome/core/log.h"
 
 
 namespace esphome {
 namespace ble_gateway {
 
 
+static const char *TAG =
+    "config_manager";
 
-class ConfigManager
+
+
+void ConfigManager::load()
+{
+
+    devices_.clear();
+
+
+    DeviceTable::load(
+        devices_
+    );
+
+
+    ESP_LOGI(
+        TAG,
+        "Config loaded:%d",
+        devices_.size()
+    );
+
+}
+
+
+
+
+bool ConfigManager::get_action(
+    const std::string &command,
+    BLEAction &action
+)
+
 {
 
 
-public:
+    size_t pos =
+        command.rfind(".");
 
 
-    void load();
+    if(
+        pos == std::string::npos
+    )
+    {
+        return false;
+    }
 
 
 
-    bool get_command(
-        const std::string &name,
-        BLEDevice &device
+    std::string device_id =
+        command.substr(
+            0,
+            pos
+        );
+
+
+    std::string action_name =
+        command.substr(
+            pos + 1
+        );
+
+
+
+    for(
+        auto &device : devices_
+    )
+    {
+
+        if(
+            device.id == device_id
+        )
+        {
+
+
+            auto it =
+                device.actions.find(
+                    action_name
+                );
+
+
+            if(
+                it != device.actions.end()
+            )
+            {
+
+                action =
+                    it->second;
+
+
+                return true;
+
+            }
+
+        }
+
+    }
+
+
+
+    ESP_LOGW(
+        TAG,
+        "action not found:%s",
+        command.c_str()
     );
 
 
 
-private:
+    return false;
 
-
-    std::vector<BLEDevice> devices_;
-
-
-
-};
+}
 
 
 
