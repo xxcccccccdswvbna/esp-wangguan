392902a02388cb4f8739be973e0e7e358bd5986a
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 23:22:47 +0800

    Update ble_light.h

--- components/ble_light/ble_light.h
+++ components/ble_light/ble_light.h
@@ -1,19 +1,17 @@
 #pragma once
 
-#include "esphome/core/component.h"
 #include "esphome/components/light/light_output.h"
 #include "../ble_gateway/ble_gateway.h"
 
 namespace esphome {
 namespace ble_light {
 
-class BLELight : public light::LightOutput, public Component {
+// 【修改】只继承 LightOutput，移除 Component
+class BLELight : public light::LightOutput {
 public:
-    // 由 YAML 配置注入的依赖
     void set_gateway(ble_gateway::BLEGateway *gateway) { gateway_ = gateway; }
     void set_device_id(const std::string &device_id) { device_id_ = device_id; }
 
-    // ESPHome LightOutput 必须实现的接口
     light::LightTraits get_traits() override;
     void write_state(light::LightState *state) override;
 
@@ -21,7 +19,6 @@ protected:
     ble_gateway::BLEGateway *gateway_{nullptr};
     std::string device_id_;
 
-    // 内部映射算法
     std::string map_brightness(float brightness);
     std::string map_color_temp(float color_temp_mireds);
 };
