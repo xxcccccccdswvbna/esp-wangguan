8dae078eb6baefe6fa130d32e6dd8d6dd18c976b
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 12:18:40 +0800

    Update light.py

--- components/ble_gateway/light.py
+++ components/ble_gateway/light.py
@@ -3,11 +3,6 @@ import esphome.config_validation as cv
 
 from esphome.components import light
 
-from esphome.const import (
-    CONF_ID,
-    CONF_NAME,
-)
-
 from . import ble_gateway_ns
 
 
@@ -21,18 +16,21 @@ BLELight = ble_gateway_ns.class_(
 CONF_DEVICE_ID = "device_id"
 
 
+
 CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend(
     {
-        cv.Required(CONF_DEVICE_ID): cv.string,
+        cv.Required(
+            CONF_DEVICE_ID
+        ): cv.string,
     }
 )
+
 
 
 async def to_code(config):
 
     var = cg.new_Pvariable(
-        config[CONF_ID],
-        BLELight
+        config[cv.GenerateID()]
     )
 
 
