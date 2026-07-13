f5ac884a745a0abce8ce6f6a26f7a4b86e4bf9ee
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 10:21:46 +0800

    Create device_model.h

---
+++ components/ble_gateway/device_model.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+
+
+namespace esphome {
+namespace ble_gateway {
+
+
+struct BLEAction
+{
+
+    std::string name;
+
+
+    std::vector<std::string> packets;
+
+};
+
+
+
+struct BLEDevice
+{
+
+    // HA实体ID
+    std::string id;
+
+
+    // light / fan
+    std::string type;
+
+
+    // 显示名称
+    std::string name;
+
+
+
+    // 动作列表
+    std::map<std::string, BLEAction> actions;
+
+
+};
+
+
+
+}
+}
