95a54ee30a116a44a2cb1b6f52cb45e677839751
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 23:24:55 +0800

    Update light.py

--- components/ble_light/light.py
+++ components/ble_light/light.py
@@ -6,7 +6,8 @@ from esphome.const import CONF_ID
 DEPENDENCIES = ["ble_gateway"]
 
 ble_light_ns = cg.esphome_ns.namespace("ble_light")
-ble_gateway_ns = cg.esphome.ns.namespace("ble_gateway")
+# 【修正】这里必须是 cg.esphome_ns，不能是 cg.esphome.ns
+ble_gateway_ns = cg.esphome_ns.namespace("ble_gateway")
 
 BLELight = ble_light_ns.class_("BLELight", light.LightOutput)
 BLEGateway = ble_gateway_ns.class_("BLEGateway", cg.Component)
@@ -23,7 +24,6 @@ CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     
-    # 【修改】只注册为 Light，不再注册为 Component
     await light.register_light(var, config)
 
     gateway_var = await cg.get_variable(config[CONF_GATEWAY])
