f329f1329e12a52037d774a8e348f317edb4b815
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-14 00:27:40 +0800

    Update fan.py

--- components/ble_fan/fan.py
+++ components/ble_fan/fan.py
@@ -1,36 +1,37 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import fan
-from esphome.const import CONF_OUTPUT_ID, CONF_NAME, CONF_ID
+# 【修正】使用 CONF_ID 而不是 CONF_OUTPUT_ID
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["ble_gateway"]
 
 ble_fan_ns = cg.esphome_ns.namespace("ble_fan")
 ble_gateway_ns = cg.esphome_ns.namespace("ble_gateway")
 
-# 声明 C++ 类
+# BLEFan 继承自 fan::Fan (即 FanState)，所以直接绑定到 CONF_ID
 BLEFan = ble_fan_ns.class_("BLEFan", cg.Component, fan.Fan)
 BLEGateway = ble_gateway_ns.class_("BLEGateway", cg.Component)
 
 CONF_BLE_DEVICE_ID = "ble_device_id"
 CONF_GATEWAY = "gateway"
 
-# 【修正】不再使用 fan.FAN_SCHEMA，直接使用 cv.Schema 并包含 fan 必须的字段
+# 【修正】使用 cv.GenerateID() 生成 CONF_ID，满足 fan.register_fan 的要求
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(BLEFan),
+    cv.GenerateID(): cv.declare_id(BLEFan),
     cv.Required(CONF_NAME): cv.string,
     cv.Required(CONF_BLE_DEVICE_ID): cv.string,
     cv.Required(CONF_GATEWAY): cv.use_id(BLEGateway),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    # 实例化 BLEFan 对象
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    # 【修正】使用 config[CONF_ID] 实例化 BLEFan
+    var = cg.new_Pvariable(config[CONF_ID])
     
     # 注册为 Component
     await cg.register_component(var, config)
     
-    # 注册为 Fan 实体 (这会自动处理 HA 的实体注册)
+    # 注册为 Fan 实体 (此时 config 中已有 CONF_ID，不会报 KeyError)
     await fan.register_fan(var, config)
 
     # 注入依赖
