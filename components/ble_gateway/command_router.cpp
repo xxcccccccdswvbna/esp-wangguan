258055f4c147399109610c5a2797f03a2d6f19e1
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 21:16:34 +0800

    Create command_router.cpp

---
+++ components/ble_gateway/command_router.cpp
@@ -0,0 +1,64 @@
+#include "command_router.h"
+
+
+namespace esphome {
+namespace ble_gateway {
+
+
+bool CommandRouter::send_command(
+    std::string device,
+    std::string action
+)
+{
+
+
+    if(
+        !gateway_ ||
+        !config_
+    )
+    {
+        return false;
+    }
+
+
+
+    BLEAction result;
+
+
+
+    if(
+        !config_->get_action(
+            device,
+            action,
+            result
+        )
+    )
+    {
+        return false;
+    }
+
+
+
+    for(
+        auto &packet :
+        result.packets
+    )
+    {
+
+        gateway_->send_hex(
+            packet
+        );
+
+    }
+
+
+
+    return true;
+
+
+}
+
+
+
+}
+}
