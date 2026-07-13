8853b81595c2010dda4b54e2d573aef93b2e705d
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-14 00:17:37 +0800

    Create fan.py

---
+++ components/ble_fan/fan.py
@@ -0,0 +1,31 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import fan
+from esphome.const import CONF_OUTPUT_ID
+
+DEPENDENCIES = ["ble_gateway"]
+
+ble_fan_ns = cg.esphome_ns.namespace("ble_fan")
+ble_gateway_ns = cg.esphome_ns.namespace("ble_gateway")
+
+BLEFan = ble_fan_ns.class_("BLEFan", cg.Component, fan.Fan)
+BLEGateway = ble_gateway_ns.class_("BLEGateway", cg.Component)
+
+CONF_BLE_DEVICE_ID = "ble_device_id"
+CONF_GATEWAY = "gateway"
+
+CONFIG_SCHEMA = fan.FAN_SCHEMA.extend({
+    cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(BLEFan),
+    cv.Required(CONF_BLE_DEVICE_ID): cv.string,
+    cv.Required(CONF_GATEWAY): cv.use_id(BLEGateway),
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    
+    await cg.register_component(var, config)
+    await fan.register_fan(var, config)
+
+    gateway_var = await cg.get_variable(config[CONF_GATEWAY])
+    cg.add(var.set_gateway(gateway_var))
+    cg.add(var.set_device_id(config[CONF_BLE_DEVICE_ID]))
