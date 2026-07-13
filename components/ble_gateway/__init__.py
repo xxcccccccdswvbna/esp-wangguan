5e23958b8e2a1ae49051a407d5b0b660b93b7fa9
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-13 12:18:24 +0800

    Update __init__.py

--- components/ble_gateway/__init__.py
+++ components/ble_gateway/__init__.py
@@ -1,5 +1,28 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
 
-CODEOWNERS = []
 
-ble_gateway_ns = cg.esphome_ns.namespace("ble_gateway")
+DEPENDENCIES = [
+    "esp32_ble_tracker"
+]
+
+
+ble_gateway_ns = cg.esphome_ns.namespace(
+    "ble_gateway"
+)
+
+
+BLEGateway = ble_gateway_ns.class_(
+    "BLEGateway",
+    cg.Component
+)
+
+
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(
+            BLEGateway
+        ),
+    }
+)
