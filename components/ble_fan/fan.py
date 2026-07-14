296d33158fb223183bcd1a833b385203fd82c651
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-07-14 23:54:47 +0800

    Update fan.py

--- components/ble_fan/fan.py
+++ components/ble_fan/fan.py
@@ -1,34 +1,30 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import fan
-from esphome.const import CONF_ID, CONF_RESTORE_MODE
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["ble_gateway"]
 
-ble_fan_ns = cg.esphome_ns.namespace("ble_fan")
-ble_gateway_ns = cg.esphome_ns.namespace("ble_gateway")
-fan_ns = cg.esphome_ns.namespace("fan")
+ns = cg.esphome_ns.namespace("ble_fan")
+gw_ns = cg.esphome_ns.namespace("ble_gateway")
 
-BLEFan = ble_fan_ns.class_("BLEFan", cg.Component, fan.Fan)
-BLEGateway = ble_gateway_ns.class_("BLEGateway", cg.Component)
+BLEFan = ns.class_("BLEFan", cg.Component, fan.Fan)
+BLEGateway = gw_ns.class_("BLEGateway", cg.Component)
 
 CONF_BLE_DEVICE_ID = "ble_device_id"
 CONF_GATEWAY = "gateway"
 
-FanRestoreMode = fan_ns.namespace("FanRestoreMode")
-RESTORE_MODES = {
-    "NO_RESTORE": FanRestoreMode.NO_RESTORE,
-    "RESTORE_DEFAULT_OFF": FanRestoreMode.RESTORE_DEFAULT_OFF,
-    "RESTORE_DEFAULT_ON": FanRestoreMode.RESTORE_DEFAULT_ON,
-    "RESTORE_INVERTED_DEFAULT_OFF": FanRestoreMode.RESTORE_INVERTED_DEFAULT_OFF,
-    "RESTORE_INVERTED_DEFAULT_ON": FanRestoreMode.RESTORE_INVERTED_DEFAULT_ON,
-    "ALWAYS_OFF": FanRestoreMode.ALWAYS_OFF,
-    "ALWAYS_ON": FanRestoreMode.ALWAYS_ON,
-}
-
-_DEFAULT_RESTORE = "RESTORE_DEFAULT_OFF"
-
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BLEFan),
     cv.Required(CONF_BLE_DEVICE_ID): cv.string,
-    cv.Required(CONF_
+    cv.Required(CONF_GATEWAY): cv.use_id(BLEGateway),
+}).extend(cv.COMPONENT_SCHEMA).extend(fan.FAN_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await fan.register_fan(var, config)
+    gw = await cg.get_variable(config[CONF_GATEWAY])
+    cg.add(var.set_gateway(gw))
+    cg.add(var.set_device_id(config[CONF_BLE_DEVICE_ID]))
