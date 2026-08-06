d43a4f2ce3d69532a9c79e5195badbdf149a1f7b
xxcccccccdswvbna <124717005+xxcccccccdswvbna@users.noreply.github.com>
2026-08-06 14:27:40 +0800

    Update device_model.h

--- components/ble_gateway/device_model.h
+++ components/ble_gateway/device_model.h
@@ -7,18 +7,17 @@
 namespace esphome {
 namespace ble_gateway {
 
-// 单个动作的数据结构
 struct BLEAction {
     std::string name;
     std::vector<std::string> packets;
 };
 
-// 单个 BLE 设备的数据结构
 struct BLEDevice {
     std::string id;
     std::string type;
     std::string name;
-    // 每个设备的动作数量通常很少，使用 std::map 便于按键名快速查找和调试
+    std::string mac;       // 🔥 新增
+    std::string protocol;  // 🔥 新增
     std::map<std::string, BLEAction> actions;   
 };
 
